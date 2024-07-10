2184573708b7e0bde3730a096440b22ef3baa9f0
Mohamed Reda <mohamedredazhar2003@gmail.com>
2024-07-10 15:04:51 +0100

    added via upload

---
+++ tech_e_commerce/info_tel_e_commerce/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class InfoTelECommerceConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "info_tel_e_commerce"
---
+++ tech_e_commerce/info_tel_e_commerce/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path,include
+from . import views
+
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("cart/",views.cart,name="cart"),
+    path("checkout/",views.checkout,name="checkout"),
+    path("",views.store,name="Our store"),
+    path("update_item/",views.updateItem,name='update_item'),
+    path("process_order/",views.processOrder, name='process_order')
+    
+]
+urlpatterns+= static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
